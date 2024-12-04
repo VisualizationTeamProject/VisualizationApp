c4a2c4fbe221a712e7f8d9e9985a6d45d5950f51
Igor Stalmach <igorstalmach@outlook.com>
2024-12-04 22:58:54 +0100

    Add new models

--- ReactVision/starter-kit/components/models/modelMapping.tsx
+++ ReactVision/starter-kit/components/models/modelMapping.tsx
@@ -3,7 +3,10 @@
   const modelMapping: { [key: string]: any } = {
     'Bambo_House': require('../../models/Bambo_House.obj'),
     'building_04': require('../../models/building_04.obj'),
-    'Cyprys_House': require('../../models/Cyprys_House.obj')
+    'Cyprys_House': require('../../models/Cyprys_House.obj'),
+    'house1': require('../../models/house1.obj'),
+    'house1_open1': require('../../models/house1_open1.obj'),
+    'house1_open2': require('../../models/house1_open2.obj')
   };
 
   export default modelMapping;
