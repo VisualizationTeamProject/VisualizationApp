44424ad6de3246f653a688c70ccb86c0f6ae24b9
Igor Stalmach <igorstalmach@outlook.com>
2025-01-15 01:52:06 +0100

    Add and rename models

--- ReactVision/starter-kit/components/assets/assetMapping.tsx
+++ ReactVision/starter-kit/components/assets/assetMapping.tsx
@@ -1,11 +1,14 @@
+const assetMapping: { [key: string]: any } = {
+  'bamboo_house.png': require('../../models/bamboo_house/bamboo_house.png'),
+  'cyprys_house.png': require('../../models/cyprys_house/cyprys_house.png'),
+  'big_building.png': require('../../models/big_building/big_building.png'),
+  'castle.png': require('../../models/castle/castle.png'),
+  'family_house.png': require('../../models/family_house/family_house.png'),
+  'family_house_open.png': require('../../models/family_house/family_house_open.png'),
+  'modern_house.png': require('../../models/modern_house/modern_house.png'),
+  'modern_house_open_1.png': require('../../models/modern_house/modern_house_open_1.png'),
+  'modern_house_open_2.png': require('../../models/modern_house/modern_house_open_2.png'),
+  'shed.png': require('../../models/shed/shed.png'),
+};
 
-  const assetMapping: { [key: string]: any } = {
-    'Bambo_House.png': require('../../assets/default.png'),
-    'Cyprys_House.png': require('../../assets/Cyprys_House.png'),
-    'building_04.png': require('../../assets/building_04.png'),
-    'house1.png': require('../../assets/default.png'),
-    'house1_open1.png': require('../../assets/default.png'),
-    'house1_open2.png': require('../../assets/default.png')
-  };
-
-  export default assetMapping;
+export default assetMapping;
--- ReactVision/starter-kit/components/models/modelMapping.tsx
+++ ReactVision/starter-kit/components/models/modelMapping.tsx
@@ -1,12 +1,15 @@
-
   // Mapping of model names to 'require' statements
   const modelMapping: { [key: string]: any } = {
-    'Bambo_House': require('../../models/Bambo_House.obj'),
-    'Cyprys_House': require('../../models/Cyprys_House.obj'),
-    'building_04': require('../../models/building_04.obj'),
-    'house1': require('../../models/house1.obj'),
-    'house1_open1': require('../../models/house1_open1.obj'),
-    'house1_open2': require('../../models/house1_open2.obj')
+    'Bamboo House': require('../../models/bamboo_house/bamboo_house.obj'),
+    'Cyprys House': require('../../models/cyprys_house/cyprys_house.obj'),
+    'Big Building': require('../../models/big_building/big_building.obj'),
+    'Castle': require('../../models/castle/castle.obj'),
+    'Family House': require('../../models/family_house/family_house.obj'),
+    'Family House (No roof)': require('../../models/family_house/family_house_open.obj'),
+    'Modern House': require('../../models/modern_house/modern_house.obj'),
+    'Modern House (No roof)': require('../../models/modern_house/modern_house_open_1'),
+    'Modern House (No top floor)': require('../../models/modern_house/modern_house_open_2'),
+    'Shed': require('../../models/shed/shed.obj'),
   };
 
   export default modelMapping;
