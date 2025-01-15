63b3c68800d9e171fbb81c128c496b6116b274d1
Igor Stalmach <igorstalmach@outlook.com>
2025-01-15 21:07:23 +0100

    Fix model images

--- ReactVision/starter-kit/components/assets/assetMapping.tsx
+++ ReactVision/starter-kit/components/assets/assetMapping.tsx
@@ -6,16 +6,16 @@
     'cyprys_house.png': require('../../assets/cyprys_house.png'),
     'family_house.png': require('../../assets/family_house.png'),
     'family_house_open.png': require('../../assets/family_house_open.png'),
-    'gropius_house_habitatnow.png': require('../../assets/default.png'),
+    'gropius_house_habitatnow.png': require('../../assets/gropius_house_habitatnow.png'),
     'modern_house.png': require('../../assets/modern_house.png'),
     'modern_house_open_1.png': require('../../assets/modern_house_open_1.png'),
     'modern_house_open_2.png': require('../../assets/modern_house_open_2.png'),
-    'pawilon_barcelona_habitatnow.png': require('../../assets/default.png'),
+    'pawilon_barcelona_habitatnow.png': require('../../assets/pawilon_barcelona_habitatnow.png'),
     'shed.png': require('../../assets/shed.png'),
     'shed_open.png': require('../../assets/shed_open.png'),
-    'steiner_house_habitatnow.png': require('../../assets/default.png'),
-    'the_glass_house_habitatnow.png': require('../../assets/default.png'),
-    'villa_savoye_habitatnow.png': require('../../assets/default.png')
+    'steiner_house_habitatnow.png': require('../../assets/steiner_house_habitatnow.png'),
+    'the_glass_house_habitatnow.png': require('../../assets/the_glass_house_habitatnow.png'),
+    'villa_savoye_habitatnow.png': require('../../assets/villa_savoye_habitatnow.png')
   };
 
   export default assetMapping;
