67f447447c8c617bf56bbf531e6cb66250559308
Igor Stalmach <igorstalmach@outlook.com>
2025-01-15 02:13:00 +0100

    Add HabitatNOW models

--- ReactVision/starter-kit/components/assets/assetMapping.tsx
+++ ReactVision/starter-kit/components/assets/assetMapping.tsx
@@ -9,6 +9,11 @@ const assetMapping: { [key: string]: any } = {
   'modern_house_open_1.png': require('../../models/modern_house/modern_house_open_1.png'),
   'modern_house_open_2.png': require('../../models/modern_house/modern_house_open_2.png'),
   'shed.png': require('../../models/shed/shed.png'),
+  'gropius_house.png': require('../../models/gropius_house/gropius_house.png'),
+  'pawilon_barcelona.png': require('../../models/pawilon_barcelona/pawilon_barcelona.png'),
+  'steiner_house.png': require('../../models/steiner_house/steiner_house.png'),
+  'the_glass_house.png': require('../../models/the_glass_house/the_glass_house.png'),
+  'villa_savoye.png': require('../../models/villa_savoye/villa_savoye.png'),
 };
 
 export default assetMapping;
--- ReactVision/starter-kit/components/models/modelMapping.tsx
+++ ReactVision/starter-kit/components/models/modelMapping.tsx
@@ -10,6 +10,11 @@
     'Modern House (No roof)': require('../../models/modern_house/modern_house_open_1'),
     'Modern House (No top floor)': require('../../models/modern_house/modern_house_open_2'),
     'Shed': require('../../models/shed/shed.obj'),
+    'Gropius House': require('../../models/gropius_house/gropius_house.obj'),
+    'Barcelona Pavilion': require('../../models/pawilon_barcelona/pawilon_barcelona.obj'),
+    'Steiner House': require('../../models/steiner_house/steiner_house.obj'),
+    'The Glass House': require('../../models/the_glass_house/the_glass_house.obj'),
+    'Villa Savoye': require('../../models/villa_savoye/villa_savoye.obj'),
   };
 
   export default modelMapping;
